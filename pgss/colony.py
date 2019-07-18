b285735f73e7b231e1723c77ef7eae5ca3e48342
Andrew McGuier <amcguier@gmail.com>
2019-07-18 14:30:35 -0400

    Initial checkin of your project structure

---
+++ pgss/colony.py
@@ -0,0 +1,7 @@
+class Colony:
+    _colony_size = 10000
+    def __init__(self):
+        # todo set local variables here
+
+        pass
+    
---
+++ simulation.py
@@ -0,0 +1,13 @@
+from pgss.colony import Colony
+from pgss.analyze import ColonyAnalyzer
+from pgss.cell import Cell
+from pgss.update_colony import ColonyUpdater
+
+
+
+def runSimulation():
+    #Do your stuff here
+    pass
+
+if __name__ == '__main__':
+    runSimulation()
