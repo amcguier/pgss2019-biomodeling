77ad2d9749dc5cea79f438b361deb94e76790185
Lucy Mettee <lmettee@andrew.cmu.edu>
2019-07-18 16:18:39 -0400

    we made cells
    Created a loop to add initial amount of cells

--- pgss/colony.py
+++ pgss/colony.py
@@ -1,7 +1,45 @@
+from cell import Cell
+import random
 class Colony:
-    _colony_size = 10000
+    _colony_size = 10
     def __init__(self):
         # todo set local variables here
-
-        pass
+        
+        
+        INITIAL_RESISTANCE_CHANCE = 10000 # 1/10,000
+#add the # of cells we want into the colony value
+        #run a random percent to get a True/False value assigned to each
+        
+        
+        self.cells = []
+        
+        
+        
+        
+        for i in range(0,self._colony_size):
+            
+            
+            
+            
+            
+            
+            new_cell = None
+        
     
+            
+            
+            
+            
+            if random.randint(0,INITIAL_RESISTANCE_CHANCE) == 2:
+                new_cell = Cell() #resistant=True
+            else:
+                new_cell = Cell() #resistant=False
+            
+            
+            
+            
+            self.cells.append(new_cell)
+            print(new_cell)
+    #return colony
+
+# Cell.resistant = True/False
