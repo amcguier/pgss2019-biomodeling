48a6241445d25118f7024178546e08a93512d1ee
Anisha Sonti <asonti@andrew.cmu.edu>
2019-07-25 14:43:48 -0400

    Revert "Sample code for color density graph"
    This reverts commit a9c8d6bf0d6a1c603881b53e526d42ad21191e48.

--- pgss/analyze.py
+++ pgss/analyze.py
@@ -21,18 +21,3 @@ class ColonyAnalyzer:
         with open('colony_analysis.csv', mode = 'w') as colony_analysis:
             file_writer = csv.writer(colony_analysis, delimiter = ", ", quotechar = '"', quoting = csv.QUOTE_MINIMAL)
             ## loop command to write elements of array/list --> file_writer.writerow()
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import gaussian_kde
-
-# These are just sample numbers, the actual data needs to be inputted once found 
-x = np.random.normal(size=1000)
-y = x * 3 + np.random.normal(size=1000)
-
-# Calculate the point density
-xy = np.vstack([x,y])
-z = gaussian_kde(xy)(xy)
-
-fig, ax = plt.subplots()
-ax.scatter(x, y, c=z, s=100, edgecolor='')
-plt.show()
