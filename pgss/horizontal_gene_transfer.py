2eb7c946690692f797621444e789b55d14338c68
Lucy Mettee <lmettee@andrew.cmu.edu>
2019-07-25 17:28:48 -0400

    Update hgt
    fixed syntax

--- pgss/horizontal_gene_transfer.py
+++ pgss/horizontal_gene_transfer.py
@@ -1,6 +1,7 @@
 #file for housing gene transfer
 from pgss.cell import Cell
 from pgss.colony import Colony
+from pgss.update_colony import update_colony
 
 class Horizontal_Gene_Transfer:
     hgt_probability = 1000 #Probability of hgt
@@ -11,20 +12,26 @@ class Horizontal_Gene_Transfer:
     #make another for loop for cells that have the transfer turned on
     #run through the list of all, if they are turned on, then check the cells
 
-    for i in (0,Colony._colony_size)
-        if Cell.resistant = True
+    for i in (0,Colony._colony_size):
+        if Cell.resistant = True:
             self.resistant_index.append(i)
 
-        else: None
-
-    for i in (0,self.resistant_index)
+    for i in (0,self.resistant_index):
         x = random.randint(0,self.hgt_probability)
-        if x == 2
-            #run transfer left
+        if x == 2:
+            if self.resistant_index[i] > 0:
+                self.transfer_plasmid(self, colony,self.resistant_index[i],self.resistant_index[i]-1) #run transfer left
+
         else:
-            if x == 53
-            #run transfer right
-            else: None
+            if x == 53:
+                if self.resistant_index[i]<= Colony._colony_size:
+                    updateColony.transfer_plasmid(self, colony,self.resistant_index[i],self.resistant_index[i]+1) #run transfer right
+
+
+#Todo: add end behavior
+self.resistant_index = []
+
+
 
 
 
